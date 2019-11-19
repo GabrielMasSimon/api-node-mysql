e5de9fed3258cd3032a25892b6e9eacbeca49d01
gabriel <gabiel.mas@sm2baleares.es>
2019-11-19 12:03:51 +0100

    Añadido color a la consola

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser')
 // TODO mirar si el require es correcto o se tiene que hacer de otra forma
 const database = require('./database/database');
+const colors = require('colors');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -12,7 +13,7 @@ app.use(require('./routes/tattooStudio'));
 
 
 app.listen(3000, () => {
-    console.log('server listen on port 3000');
+    console.log(' \n Server listen on port 3000 \n'.green);
 });
 
 
