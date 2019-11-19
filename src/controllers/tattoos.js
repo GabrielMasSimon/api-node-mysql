db2d10f5d881e8e0a6969e93b47300240ff0b040
gabriel <gabiel.mas@sm2baleares.es>
2019-11-19 13:26:06 +0100

    deleted deprecated variable

--- src/controllers/tattoos.js
+++ src/controllers/tattoos.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const database = require('../database/database');
 const tattooService = require('../services/tattooService')
 
 router.get('/', (req, res) => {
